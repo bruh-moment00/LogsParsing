be390d6467bb0132f6794f7a01d765a6de49af49
bruh-moment00 <nikitabelokon@mail.ru>
2024-04-08 18:51:48 +0300

    Added tests for object

--- LogsParsing/LogsParsing.Tests/Objects/RequestLogTests.cs
+++ LogsParsing/LogsParsing.Tests/Objects/RequestLogTests.cs
@@ -1,11 +1,31 @@
+using LogsParsing.Objects;
+using System.Net;
+
 namespace LogsParsing.Tests.Objects
 {
     [TestClass]
     public class RequestLogTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TryCreateRequestLog_FromSpecificObjects()
         {
+            IPAddress testAddress = IPAddress.Parse("127.0.0.0");
+            DateTime testTime = DateTime.Parse("2024-04-08 12:00");
+
+            RequestLog requestLog = new RequestLog(testAddress, testTime);
+
+            Assert.IsNotNull(requestLog);
+        }
+
+        [TestMethod]
+        public void TryCreateRequestLog_FromText()
+        {
+            string testAddress = "127.0.0.0";
+            string testTime = "2024-04-08 12:00";
+
+            RequestLog requestLog = new RequestLog(testAddress, testTime);
+
+            Assert.IsNotNull(requestLog);
         }
     }
 }
--- LogsParsing/LogsParsing/Objects/RequestLog.cs
+++ LogsParsing/LogsParsing/Objects/RequestLog.cs
@@ -8,14 +8,14 @@ using System.Threading.Tasks;
 
 namespace LogsParsing.Objects
 {
-    internal class RequestLog
+    public class RequestLog
     {
-        internal RequestLog(IPAddress address, DateTime requestTime)
+        public RequestLog(IPAddress address, DateTime requestTime)
         {
             Address = address;
             RequestTime = requestTime;
         }
-        internal RequestLog(string address, string requestTime)
+        public RequestLog(string address, string requestTime)
         {
             Address = IPAddress.Parse(address);
             RequestTime = DateTime.Parse(requestTime);
