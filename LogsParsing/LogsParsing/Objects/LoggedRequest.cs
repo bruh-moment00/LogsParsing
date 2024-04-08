b1c0c676a6e3120deefd002bea08d8fa3a4ce8c6
bruh-moment00 <nikitabelokon@mail.ru>
2024-04-08 18:32:24 +0300

    Object class created

---
+++ LogsParsing/LogsParsing/Objects/LoggedRequest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogsParsing.Objects
+{
+    internal class LoggedRequest
+    {
+        internal LoggedRequest(IPAddress address, DateTime requestTime)
+        {
+            Address = address;
+            RequestTime = requestTime;
+        }
+        internal LoggedRequest(string address, string requestTime)
+        {
+            Address = IPAddress.Parse(address);
+            RequestTime = DateTime.Parse(requestTime);
+        }
+        public IPAddress Address { get; set; }
+        public DateTime RequestTime {  get; set; }
+    }
+}
